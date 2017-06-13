f2d623ced5b45fddfe74d0212d647331e2b9efb2
Cory Gwin @gwincr11 <gwincr11@github.com>
2017-06-13 09:31:23 -0400

    routes with periods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/*page", to: "pages#index"
+  get "/*page", to: "pages#index", :requirements => { page: /.+/ }
+
   root to: 'pages#index'
 end
